31484857ae9330c020d65a0827fd4f90736f6fff
M ABD AZIZ ALFIAN <aalfiann@gmail.com>
2018-03-10 02:17:55 +0700

    - Update default blank page

--- src/app/modul-blank.php
+++ src/app/modul-blank.php
@@ -28,7 +28,7 @@ $datalogin = Core::checkSessions();?>
                 </div>
                 <div class="col-md-7 align-self-center">
                     <ol class="breadcrumb">
-                        <li class="breadcrumb-item"><a href="javascript:void(0)">App</a></li>
+                        <li class="breadcrumb-item"><a href="javascript:void(0)"><?php echo Core::lang('app')?></a></li>
                         <li class="breadcrumb-item active">Blank Page</li>
                     </ol>
                 </div>
