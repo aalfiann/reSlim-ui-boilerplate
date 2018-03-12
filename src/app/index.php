4e54eeab8b6a865a053c070f554f979a40ce10a5
M ABD AZIZ ALFIAN <aalfiann@gmail.com>
2018-03-12 18:43:22 +0700

    - Changed default page redirect

--- src/app/index.php
+++ src/app/index.php
@@ -3,11 +3,14 @@
 spl_autoload_register(function ($classname) {require ( $classname . ".php");});
 // Verify session, cookies and token
 $datalogin = Core::checkSessions();
-// Redirect to dashboard page
-if (Core::getRole($datalogin['token']) != '3') {
+// Redirect to role page
+$indexrole = Core::getRole($datalogin['token']);
+if ($indexrole < '3') {
     Core::goToPage('modul-dashboard.php');
-} else {
+} else if ($indexrole == '3'){
     Core::goToPage('modul-dashboard-mine.php');
-}
+} else {
+    Core::goToPage('modul-user-profile.php');
+} 
 
 ?>
