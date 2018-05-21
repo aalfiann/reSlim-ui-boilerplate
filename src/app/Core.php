0a80eb98a091fcc868cc7645a440a8f11e38331a
M ABD AZIZ ALFIAN <aalfiann@gmail.com>
2018-05-21 13:29:39 +0700

    - Fully tested to using with reSlim 1.9.1 - Now update version to 1.9.0

--- src/app/Core.php
+++ src/app/Core.php
@@ -78,7 +78,7 @@
         var $pathcache = 'cache-files';
         var $minifycache = true;
 
-        var $version = '1.8.2';
+        var $version = '1.9.0';
 
         private static $instance;
         
