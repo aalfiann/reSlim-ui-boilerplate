f4f4f18b279b90930d3f03cc8be0a02c36360f82
M ABD AZIZ ALFIAN <aalfiann@gmail.com>
2018-06-16 04:03:37 +0700

    - Update to version 1.10.2

--- src/app/Core.php
+++ src/app/Core.php
@@ -78,7 +78,7 @@
         var $pathcache = 'cache-files';
         var $minifycache = true;
 
-        var $version = '1.10.1';
+        var $version = '1.10.2';
 
         private static $instance;
         
