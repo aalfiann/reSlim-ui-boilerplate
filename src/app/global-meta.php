a7352d7cba2bae6cfd7cdfb0f0ad552fea2812e8
M ABD AZIZ ALFIAN <aalfiann@gmail.com>
2018-07-17 13:56:29 +0700

    - Update basic SEO

--- src/app/global-meta.php
+++ src/app/global-meta.php
@@ -8,6 +8,17 @@
     <link href="../assets/plugins/bootstrap/css/bootstrap.min.css" rel="stylesheet">
     <!-- Custom CSS -->
     <link href="css/style.css" rel="stylesheet">
+    <!-- Theme Color -->
+    <meta name="theme-color" content="#1976d2">
+    <!-- Additional -->
+    <link rel="canonical" href="<?php echo ((Core::isHttpsButtflare()) ? 'https' : 'http') . '://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']?>" />
+    <link rel="alternate" href="<?php echo ((Core::isHttpsButtflare()) ? 'https' : 'http') . '://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']?>" hreflang="<?php echo Core::getInstance()->setlang?>" />
+    <link rel="alternate" type="application/rss+xml" title="RSS Feed <?php echo Core::getInstance()->title?>" href="<?php echo Core::getInstance()->basepath?>/rss.php" />
+    <link rel="alternate" type="application/xml" title="Sitemap <?php echo Core::getInstance()->title?>" href="<?php echo Core::getInstance()->basepath?>/sitemap.xml" />
+    <!-- Site Verification -->
+    <meta name="google-site-verification" content="<?php echo Core::getInstance()->googlewebmaster?>" />
+    <meta name="msvalidate.01" content="<?php echo Core::getInstance()->bingwebmaster?>"/>
+    <meta name="yandex-verification" content="<?php echo Core::getInstance()->yandexwebmaster?>"/>
     
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
